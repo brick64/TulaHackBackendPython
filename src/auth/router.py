4bb4ef691458bffad2e18f83829173f189b35a07
Nikita Mittal <nikita.mittal@yandex.ru>
2025-04-18 23:29:48 +0300

    fix

---
+++ src/auth/router.py
@@ -0,0 +1,31 @@
+import code
+
+from fastapi import APIRouter, Request
+
+import requests
+
+router = APIRouter(
+    prefix="/auth",
+)
+
+CLIENT_ID = "m1AvMGC45JfxilxZV1nc0EjlOn7PJuWt"
+CLIENT_SECRET = "ATOANgo7barHzQqx3nmD7iqXd9pnt_RTeAuhIxZGwxInaiRYguKMWLa02dUylhadF6moC8E90AC1"
+REDIRECT_URI = "myapp://oauth2redirect"
+
+@router.post("/token")
+async def get_access_token(request: Request):
+    authorization_code = request.headers.get("Authorization")
+    response = requests.post(
+        url = "https://auth.atlassian.com/oauth/token",
+        data = {
+            "grant_type": "authorization_code",
+            "client_id":CLIENT_ID,
+            "client_secret":CLIENT_SECRET,
+            "code":authorization_code,
+            "redirect_uri":REDIRECT_URI,
+        },
+    )
+    return {
+        "access_token": response.json()["access_token"],
+        "expires_in": response.json()["expires_in"],
+    }
--- src/main.py
+++ src/main.py
@@ -1,13 +1,32 @@
+import logging
+import os
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from src.common.dto.api import MessageResponse
+from src.common.storage.postgres.postgres_service import PostgresService
 
+from src.auth.router import router as auth_router
+
+
+database_url = os.environ.get("DATABASE_URI", "postgresql://postgres:postgres@postgres:5432/postgres")
+db_service = PostgresService(database_url)
+db_service.init_database()
+
+logger = logging.getLogger(__name__)
+
+#Initiate FastAPI
+app = FastAPI(title="Supernova")
+
+app.include_router(auth_router)
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def ping() -> MessageResponse:
+    return MessageResponse(message="Pong!")
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if os.getenv("DEBUG"):
+    logger.critical("!!! RUNNING IN DEBUG MODE !!!")
+    @app.post("/debug/drop")
+    def drop() -> None:
+        db_service.drop_database()
+        db_service.init_database()
